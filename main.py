916b60a7aa7ca4eaa734465bf278fc9949be6380
JKT-LTP-038 <jkt-ltp-038@JKT-LTP-038s-MacBook-Air.local>
2025-03-16 20:22:08 +0700

    Re-upload backend project

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Bisa diubah ke domain tertentu
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
